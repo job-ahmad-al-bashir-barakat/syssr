5cf28f09fb3dd465494203184e761e6548098616
abdulkareem.fallaha <abdulkareem.f@clic.agency>
2019-09-27 15:04:02 +0300

    edit readme - gitignore - change reset password table name for users

--- website/.phpstorm.meta.php
+++ website/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-09-21 01:45:19.
+    * Generated on 2019-09-24 08:14:33.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
