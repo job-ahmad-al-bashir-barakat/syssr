365118bc6e212a8b11665d565358652ba458a38c
Bashir Barakat <egypt.ahmad.al.bashir.barakat@gmail.com>
2019-11-01 14:14:52 +0200

    - Table Country And City Api

--- website/resources/views/members_society.blade.php
+++ website/resources/views/members_society.blade.php
@@ -13,64 +13,64 @@
                     <div class="brk-tables brk-tables-strict font__family-montserrat brk-form-strict" data-brk-library="component__tables,datatables_js">
                         <table>
                             <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th class="active" scope="col">Company <i class="fa fa-caret-down" aria-hidden="true"></i></th>
-                                <th scope="col">Order Date <i class="fa fa-caret-down" aria-hidden="true"></i></th>
-                                <th scope="col">Product <i class="fa fa-caret-down" aria-hidden="true"></i></th>
-                                <th scope="col">Amount <i class="fa fa-caret-down" aria-hidden="true"></i></th>
-                                <th scope="col">Total <i class="fa fa-caret-down" aria-hidden="true"></i></th>
-                            </tr>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th class="active" scope="col">Company <i class="fa fa-caret-down" aria-hidden="true"></i></th>
+                                    <th scope="col">Order Date <i class="fa fa-caret-down" aria-hidden="true"></i></th>
+                                    <th scope="col">Product <i class="fa fa-caret-down" aria-hidden="true"></i></th>
+                                    <th scope="col">Amount <i class="fa fa-caret-down" aria-hidden="true"></i></th>
+                                    <th scope="col">Total <i class="fa fa-caret-down" aria-hidden="true"></i></th>
+                                </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Lycos</td>
-                                <td>12/05/2015</td>
-                                <td>Product Six</td>
-                                <td>7</td>
-                                <td>$585.00</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Lycos</td>
-                                <td>12/05/2015</td>
-                                <td>Product Six</td>
-                                <td>7</td>
-                                <td>$585.00</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Lycos</td>
-                                <td>12/05/2015</td>
-                                <td>Product Six</td>
-                                <td>7</td>
-                                <td>$585.00</td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>Lycos</td>
-                                <td>12/05/2015</td>
-                                <td>Product Six</td>
-                                <td>7</td>
-                                <td>$585.00</td>
-                            </tr>
-                            <tr>
-                                <td>5</td>
-                                <td>Lycos</td>
-                                <td>12/05/2015</td>
-                                <td>Product Six</td>
-                                <td>7</td>
-                                <td>$585.00</td>
-                            </tr>
-                            <tr>
-                                <td>6</td>
-                                <td>Lycos</td>
-                                <td>12/05/2015</td>
-                                <td>Product Six</td>
-                                <td>7</td>
-                                <td>$585.00</td>
-                            </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Lycos</td>
+                                    <td>12/05/2015</td>
+                                    <td>Product Six</td>
+                                    <td>7</td>
+                                    <td>$585.00</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Lycos</td>
+                                    <td>12/05/2015</td>
+                                    <td>Product Six</td>
+                                    <td>7</td>
+                                    <td>$585.00</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Lycos</td>
+                                    <td>12/05/2015</td>
+                                    <td>Product Six</td>
+                                    <td>7</td>
+                                    <td>$585.00</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Lycos</td>
+                                    <td>12/05/2015</td>
+                                    <td>Product Six</td>
+                                    <td>7</td>
+                                    <td>$585.00</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>Lycos</td>
+                                    <td>12/05/2015</td>
+                                    <td>Product Six</td>
+                                    <td>7</td>
+                                    <td>$585.00</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>Lycos</td>
+                                    <td>12/05/2015</td>
+                                    <td>Product Six</td>
+                                    <td>7</td>
+                                    <td>$585.00</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
